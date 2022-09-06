1521e1802f3ddaf256f9df33a2255649a6f01864
mbiga <mbigaram.mb@gmail.com>
2022-09-06 16:09:26 -0300

    Exercicios 012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,30 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
 
-if(){
 
+
+let idade = Number(prompt("Qual a sua idade?"))
+
+if(idade >= 18){
+    alert("Você é maior de idade")
 }else{
-	
+    alert("Você é menor de idade")
+}
+
+
+
+
+let ensinoMedio = prompt("Concluiu o ensino médio?(sim ou não)")
+let isCursandoOutraFaculdade
+
+if(ensinoMedio === "sim"){
+
+    isCursandoOutraFaculdade = prompt("Você esta cursando a faculdade? (sim ou não)")
+
+    if(ensinoMedio === "sim"){
+       console.log ("Você esta cursando faculdade.")
+}else{
+       console.log ("Você não está na faculdade")
+}
+
+}else {
+    console.log("Você não concluiu o ensino médio")
 }
